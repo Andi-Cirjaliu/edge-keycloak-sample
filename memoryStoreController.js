9589441c620db2e773d03643a806c85d5aa9963f
Andi Cirjaliu <Andi.Cirjaliu@ro.ibm.com>
2021-03-08 13:58:27 +0200

    Updates

--- memoryStoreController.js
+++ memoryStoreController.js
@@ -4,7 +4,12 @@ const store1 = new session.MemoryStore();
 const store2 = new session.MemoryStore();
 
 const printStore = (store) => {
-    console.log(store.sessions);
+    console.log(store);
+}
+
+const printStores = () => {
+    console.log(store1);
+    console.log(store2);
 }
 
 module.exports = {store1, store2, printStore};
